3a41d6e3ffd87e4cadea5db2f49565f1c73a17ed
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-06-22 19:04:04 +0300

    game introduction added to hompage

--- GlobeGuide/src/pages/Hompage.jsx
+++ GlobeGuide/src/pages/Hompage.jsx
@@ -1,29 +1,62 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
 import WorldMap from './WorldMap'
 import './pagesCss/Hompage.css'
 import Footer from '../components/Footer'
+import RandomCountry from '../components/RandomCountry'
 function Hompage() {
   return (
     <>
       <Navbar />
-      <WorldMap />
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1>Explore the World with GlobeGuide</h1>
+          <p>Test your geography knowledge and discover new countries and continents!</p>
+        </div>
+        <div className="hero-map">
+          <WorldMap />
+        </div>
+      </div>
       <div className='homepage-container'>
+        <div className='games-intro-section'>
+          <h2 className='games-intro-title'>Play & Learn: GlobeGuide Games</h2>
+          <p className='games-intro-desc'>
+            Challenge yourself with our fun and educational geography games!
+          </p>
+          <div className='games-cards-container'>
+            <div className='game-card'>
+              <img className='game-card-icon' src='Flag.png' alt='Flag Guess' />
+              <h3>Flag Guess</h3>
+              <p>Can you match the flag to the country? Test your flag knowledge!</p>
+              <a href="/game/flag-guess" className="play-game-btn">Play</a>
+            </div>
+            <div className='game-card'>
+              <img className='game-card-icon' src='infoGuess.png' alt='Country Info Guess' />
+              <h3>Country Info Guess</h3>
+              <p>Guess the country from clues about its capital, region, and more.</p>
+              <a href="/game/country-info-guess" className="play-game-btn">Play</a>
+            </div>
+            <div className='game-card'>
+              <img className='game-card-icon' src='findOnMap.png' alt='Find Country on Map' />
+              <h3>Find Country on Map</h3>
+              <p>Locate countries on the world map. How well do you know geography?</p>
+              <a href="/game/find-country-on-map" className="play-game-btn">Play</a>
+            </div>
+            <div className='game-card'>
+              <img className='game-card-icon' src='guessTheCapital.png' alt='Guess the Capital' />
+              <h3>Guess the Capital</h3>
+              <p>Given a capital, can you pick the correct country? Try it now!</p>
+              <a href="/game/guess-the-capital" className="play-game-btn">Play</a>
+            </div>
+          </div>
+        </div>
+  <div className='hompage-text'>
         <h3 className='homepage-title'>Welcome to GlobeGuide</h3>
         <p className='homepage-description'>
           Explore the world through interactive maps and engaging games. Test your knowledge of countries, flags, and geography while having fun!
         </p>
+      </div>
         <div className='games-section'>
-          <div className='games-wrapper'>
-            <img className='games-img' src='https://picsum.photos/200/300'></img>
-            <p className='games-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ex ut elit aliquet auctor vitae eu sem. Morbi et.</p>
-          </div>
-
-          <div className='games-wrapper'>
-            <img className='games-img' src='https://picsum.photos/200/300'></img>
-            <p className='games-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ex ut elit aliquet auctor vitae eu sem. Morbi et.</p>
-          </div>
-
+        <div className='first-row'>
           <div className='games-wrapper'>
             <img className='games-img' src='https://picsum.photos/200/300'></img>
             <p className='games-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ex ut elit aliquet auctor vitae eu sem. Morbi et.</p>
@@ -34,6 +67,23 @@ function Hompage() {
             <p className='games-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ex ut elit aliquet auctor vitae eu sem. Morbi et.</p>
           </div>
         </div>
+        <div className='second-row'>
+          <div className='games-wrapper'>
+            <img className='games-img' src='https://picsum.photos/200/300'></img>
+            <p className='games-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ex ut elit aliquet auctor vitae eu sem. Morbi et.</p>
+          </div>
+
+          <div className='games-wrapper'>
+            <img className='games-img' src='https://picsum.photos/200/300'></img>
+            <p className='games-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ex ut elit aliquet auctor vitae eu sem. Morbi et.</p>
+          </div>
+        </div>  
+        </div>
+      </div>
+      <div className='random-country'>
+        <h3 className='random-country-title'>Random Country</h3>
+        <p className='random-country-description'>Discover a random country and learn about its culture, geography, and more!</p>
+        <RandomCountry />
       </div>
       <div className='secondary-container'>
       <img className='secondary-img' src='https://picsum.photos/200/300' alt='GlobeGuide Logo' />
