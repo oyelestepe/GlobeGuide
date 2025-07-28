39d41637d0e17a98a5536b73475086bbfec92593
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-07-28 19:26:46 +0300

    vercel deploy bug fix

--- GlobeGuide/vite.config.js
+++ GlobeGuide/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,5 +11,5 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   },
-  base: process.env.VITE_BASE_PATH || '/GlobeGuide',
+  base: '/',
 });
